b685bd782d446840ea9f736be96f2bd25ecf72a9
John Pomeroy <jpcoded@gmail.com>
2017-06-11 15:09:02 -0400

    Converted methods to expresion bodies in GetDocumentNodes.cs

--- AnimeUploader/GetDocumentNodes.cs
+++ AnimeUploader/GetDocumentNodes.cs
@@ -17,78 +17,39 @@ namespace AnimeUploader
             _nodes = JsonLoader.LoadNodeSettings();
         }
 
-        public string getType()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Type).InnerText.Replace("Type:", "").Trim();
-        }
+        public string getType() => _document.DocumentNode.SelectSingleNode(_nodes.Type).InnerText.Replace("Type:", "").Trim();
 
-        public string GetEpisode()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Episode).InnerText.Replace("Episodes:", "").Trim();
-        }
+        public string GetEpisode() => _document.DocumentNode.SelectSingleNode(_nodes.Episode).InnerText.Replace("Episodes:", "").Trim();
 
-        public string GetStatus()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Status).InnerText.Replace("Status:", "").Trim();
-        }
+        public string GetStatus() => _document.DocumentNode.SelectSingleNode(_nodes.Status).InnerText.Replace("Status:", "").Trim();
 
-        public string GetAired()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Aired).InnerText.Replace("Aired:", "").Trim();
-        }
+        public string GetAired() => _document.DocumentNode.SelectSingleNode(_nodes.Aired).InnerText.Replace("Aired:", "").Trim();
 
-        public string GetDuration()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Duration).InnerText.Replace("Duration:", "").Trim();
-        }
+        public string GetDuration() => _document.DocumentNode.SelectSingleNode(_nodes.Duration).InnerText.Replace("Duration:", "").Trim();
 
-        public string GetRating()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Rating).InnerText.Replace("Rating:", "").Trim();
-        }
+        public string GetRating() => _document.DocumentNode.SelectSingleNode(_nodes.Rating).InnerText.Replace("Rating:", "").Trim();
 
-        public string GetSynopsis()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Synopsis)
-                .GetAttributeValue("content", "")
-                .Replace("'", "''")
-                .Trim();
-        }
+        public string GetSynopsis() => _document.DocumentNode.SelectSingleNode(_nodes.Synopsis)
+            .GetAttributeValue("content", "")
+            .Replace("'", "''")
+            .Trim();
 
-        public string GetGenres()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Genres)
-                .InnerText.Replace("Genres:", "")
-                .Replace(", ", ",")
-                .Trim();
-        }
+        public string GetGenres() => _document.DocumentNode.SelectSingleNode(_nodes.Genres)
+            .InnerText.Replace("Genres:", "")
+            .Replace(", ", ",")
+            .Trim();
 
-        public string GetTitles()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Title)
-                .GetAttributeValue("content", "")
-                .Replace("'", "''")
-                .Trim();
-        }
+        public string GetTitles() => _document.DocumentNode.SelectSingleNode(_nodes.Title)
+            .GetAttributeValue("content", "")
+            .Replace("'", "''")
+            .Trim();
 
-        public HtmlNode GetPrequelId()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.PrequelId);
-        }
+        public HtmlNode GetPrequelId() => _document.DocumentNode.SelectSingleNode(_nodes.PrequelId);
 
-        public HtmlNode GetPrequel()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Prequel);
-        }
+        public HtmlNode GetPrequel() => _document.DocumentNode.SelectSingleNode(_nodes.Prequel);
 
-        public HtmlNode GetSequelId()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.SequelId);
-        }
+        public HtmlNode GetSequelId() => _document.DocumentNode.SelectSingleNode(_nodes.SequelId);
 
-        public HtmlNode GetSequel()
-        {
-            return _document.DocumentNode.SelectSingleNode(_nodes.Sequel);
-        }
+        public HtmlNode GetSequel() => _document.DocumentNode.SelectSingleNode(_nodes.Sequel);
     }
 }
